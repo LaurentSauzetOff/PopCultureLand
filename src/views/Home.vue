4dfd369d51f0a6868b32404fbc9db488043e2662
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2021-12-24 04:56:22 +0100

    Part 1 of 6 is checked

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,107 @@
 <template>
   <div class="home">
-    
+    <BlogPost v-if="!user" :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in blogPostsFeed"
+      :key="index"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>Voir les articles les plus récents</h3>
+        <div class="blog-cards">
+          <BlogCard
+            :post="post"
+            v-for="(post, index) in blogPostsCards"
+            :key="index"
+          />
+        </div>
+      </div>
+    </div>
+    <div v-if="!user" class="updates">
+      <div class="container">
+        <h2>Ne manquez aucune publication. Enregistrez-vous gratuitement !</h2>
+        <router-link class="router-button" to="#">
+          Enregistrez-vous sur PopCultureLand <Arrow class="arrow arrow-light" />
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BlogPost from "../components/BlogPost";
+import BlogCard from "../components/BlogCard";
+import Arrow from "../assets/Icons/arrow-right-light.svg";
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost, BlogCard, Arrow },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Bienvenue !",
+        blogPost:
+          "Articles de blog hebdomadaires avec tout ce qui concerne la programmation, y compris HTML, CSS, JavaScript et plus encore. Inscrivez-vous aujourd'hui pour ne manquer aucune publication !",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+    };
+  },
+  computed: {
+    blogPostsFeed() {
+      return this.$store.getters.blogPostsFeed;
+    },
+    blogPostsCards() {
+      return this.$store.getters.blogPostsCards;
+    },
+    user() {
+      return this.$store.state.user;
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
+</style>
