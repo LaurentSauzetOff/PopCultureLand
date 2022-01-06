abc6b0f02d92fa86ea0cf8cac39887781e8db658
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2022-01-06 18:09:57 +0100

    Profile menu check

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,69 +2,120 @@
   <header>
     <nav class="container">
       <div class="branding">
-        <router-link class="header" :to="{name: 'Home'}"
+        <router-link class="header" :to="{ name: 'Home' }"
           >PopCultureLand</router-link
         >
       </div>
       <div class="nav-links">
         <ul v-show="!mobile">
-          <router-link class="link" :to="{name: 'Home'}">Accueil</router-link>
-          <router-link class="link" :to="{name: 'Blogs'}">Articles</router-link>
+          <router-link class="link" :to="{ name: 'Home' }">Accueil</router-link>
+          <router-link class="link" :to="{ name: 'Blogs' }"
+            >Articles</router-link
+          >
           <router-link class="link" to="#">Ecrire un article</router-link>
-          <router-link class="link" :to="{name: 'Login'}"
+          <router-link class="link" :to="{ name: 'Login' }"
             >Connexion / Enregistrement</router-link
           >
         </ul>
+        <div @click="toggleProfileMenu" class="profile" ref="profile">
+          <span>{{ this.$store.state.profilInitials }}</span>
+          <div v-show="profileMenu" class="profile-menu">
+            <div class="info">
+              <p class="initials">{{ this.$store.state.profilInitials }}</p>
+              <div class="right">
+                <p>
+                  {{ this.$store.state.profileFirstName }}
+                  {{ this.$store.state.profileLastName }}
+                </p>
+                <p>{{ this.$store.state.profileUserName }}</p>
+                <p>{{ this.$store.state.profileEmail }}</p>
+              </div>
+            </div>
+            <div class="options">
+              <div class="option">
+                <router-link class="option" to="#">
+                  <userIcon class="icon" />
+                  <p>Profil</p>
+                </router-link>
+              </div>
+              <div class="option">
+                <router-link class="option" to="#">
+                  <adminIcon class="icon" />
+                  <p>Admin</p>
+                </router-link>
+              </div>
+              <div @click="signOut" class="option">
+                
+                  <signOutIcon class="icon" />
+                  <p>Déconnexion</p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
-    <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
+    <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile" />
     <transition name="mobile-nav">
-      
-        <ul class="mobile-nav" v-show="mobileNav">
-          <router-link class="link" :to="{name: 'Home'}">Accueil</router-link>
-          <router-link class="link" :to="{name: 'Blogs'}">Articles</router-link>
-          <router-link class="link" to="#">Ecrire un article</router-link>
-          <router-link class="link" :to="{name: 'Login'}"
-            >Connexion / Inscription</router-link
-          >
-        </ul>
-      
+      <ul class="mobile-nav" v-show="mobileNav">
+        <router-link class="link" :to="{ name: 'Home' }">Accueil</router-link>
+        <router-link class="link" :to="{ name: 'Blogs' }">Articles</router-link>
+        <router-link class="link" to="#">Ecrire un article</router-link>
+        <router-link class="link" :to="{ name: 'Login' }"
+          >Connexion / Inscription</router-link
+        >
+      </ul>
     </transition>
   </header>
 </template>
 
 <script>
-import menuIcon from '../assets/Icons/bars-regular.svg'
+import menuIcon from "../assets/Icons/bars-regular.svg";
+import userIcon from "../assets/Icons/user-alt-light.svg";
+import adminIcon from "../assets/Icons/user-crown-light.svg";
+import signOutIcon from "../assets/Icons/sign-out-alt-regular.svg";
+import firebase from "firebase/app";
+import "firebase/auth";
 export default {
-name: 'navigation',
-components: {menuIcon},
-data(){
+  name: "navigation",
+  components: { menuIcon, userIcon, adminIcon, signOutIcon },
+  data() {
     return {
-        mobile: null,
-        mobileNav: null,
-        windowWidth: null,
-    }
-},
-created(){
-    window.addEventListener('resize', this.checkScreen);
-    this.checkScreen
-},
-methods: {
-    checkScreen(){
-        this.windowWidth = window.innerWidth;
-        if (this.windowWidth <= 750){
-            this.mobile = true;
-            return
-        }
-        this.mobile = false;
-        this.mobileNav = false;
-        return
+      profileMenu: null,
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.checkScreen);
+    this.checkScreen;
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+      return;
     },
-    toggleMobileNav(){
-        this.mobileNav = !this.mobileNav;
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav;
+    },
+    toggleProfileMenu(e) {
+      if (e.target === this.$refs.profile) {
+        this.profileMenu = !this.profileMenu;
+      }
+    },
+    signOut(){
+      firebase.auth().signOut();
+      window.location.reload()
     }
-}
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -76,7 +127,7 @@ header {
   z-index: 99;
 
   .link {
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     font-weight: 400;
     padding: 0 8px;
     transition: 0.3s color ease;
@@ -89,7 +140,7 @@ header {
   nav {
     display: flex;
     padding: 25px 0;
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
 
     .branding {
       display: flex;
@@ -119,6 +170,90 @@ header {
 
         .link:last-child {
           margin-right: 0;
+        }
+      }
+
+      .profile {
+        position: relative;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        color: white;
+        background-color: #303030;
+
+        span {
+          pointer-events: none;
+        }
+        .profile-menu {
+          position: absolute;
+          top: 60px;
+          right: 0;
+          width: 250px;
+          background-color: #303030;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+          .info {
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            border-bottom: 1px solid white;
+
+            .initials {
+              position: initial;
+              width: 40px;
+              height: 40px;
+              background-color: #fff;
+              color: #303030;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              border-radius: 50%;
+            }
+            .right {
+              flex: 1;
+              margin-left: 24px;
+
+              p:nth-child(1) {
+                font-size: 14px;
+              }
+
+              p:nth-child(2),
+              p:nth-child(3) {
+                font-size: 12px;
+              }
+            }
+          }
+
+          .options {
+            padding: 15px;
+
+            .option {
+              text-decoration: none;
+              color: #fff;
+              display: flex;
+              align-items: center;
+              margin-bottom: 12px;
+
+              .icon {
+                width: 18px;
+                height: auto;
+              }
+
+              p {
+                font-size: 14px;
+                margin-left: 12px;
+              }
+
+              &:last-child {
+                margin-bottom: 0px;
+              }
+            }
+          }
         }
       }
     }
@@ -152,22 +287,20 @@ header {
   }
 
   .mobile-nav-enter-active,
-  .mobile-nav-leave-active{
-      transition: all 1s ease;
-
-
+  .mobile-nav-leave-active {
+    transition: all 1s ease;
   }
 
-  .mobile-nav-enter{
-      transform: translateX(-281px);
+  .mobile-nav-enter {
+    transform: translateX(-281px);
   }
 
-  .mobile-nav-enter-to{
-      transform: translateX(0);
+  .mobile-nav-enter-to {
+    transform: translateX(0);
   }
 
-  .mobile-nav-leave-to{
-      transform: translateX(-281px);
+  .mobile-nav-leave-to {
+    transform: translateX(-281px);
   }
 }
 </style>
